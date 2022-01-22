14951e5995dad22c818333a2a3889834663ebf27
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2022-01-21 17:10:19 -0800

    Cleaned up script

--- ImportAndShareScript/ImportAndShareScript.py
+++ ImportAndShareScript/ImportAndShareScript.py
@@ -53,7 +53,7 @@ def run(context):
                     step_options = import_manager.createSTEPImportOptions(file_path)
                     new_document = import_manager.importToNewDocument(step_options)
                     new_document.saveAs(file_name, target_data_folder, 'Imported from script', 'tag')
-                    imported_filenames.append(new_document.name)
+                    imported_filenames.append(file_name)
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
@@ -64,19 +64,19 @@ class MyDataFileCompleteHandler(adsk.core.DataEventHandler):
         super().__init__()
 
     def notify(self, args: adsk.core.DataEventArgs):
-        if args.filename in imported_filenames:
+        if args.file.name in imported_filenames:
             data_file: adsk.core.DataFile = args.file
             document = app.documents.open(data_file)
             public_link = data_file.publicLink
             with open(csv_file_name, mode='a') as csv_file:
-                fieldnames = ['Name', 'URN', 'link']
+                fieldnames = ['Name', 'URN', 'Link']
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                 writer.writerow({
                     'Name': data_file.name,
                     'URN': data_file.versionId,
                     'Link': public_link
                 })
-            imported_filenames.remove(args.filename)
+            imported_filenames.remove(args.file.name)
             document.close(False)
 
 
